01f7c60a61d2682093e0f6d5661197cf7f9f4053
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-07-12 06:15:00 -0400

    new approach

--- popup.js
+++ popup.js
@@ -1,18 +1,9 @@
-let scrapeEmails = document.getElementById("scrapeEmails");
+document.addEventListener('DOMContentLoaded', function() {
+  var button = document.getElementById('gradesButton');
 
-scrapeEmails.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: displayGrades,
+  button.addEventListener('click', function() {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'displayGrades' });
+    });
   });
 });
-
-function displayGrades() {
-  const gradeInputs = document.querySelectorAll(".u7S8tc .ksaOtd");
-  // Create an array to store the grade values
-  const gradeValues = [];
-  //iterate through the values and push them to the gradeValues array
-  gradeInputs.forEach((gradeInput) => gradeValues.push(gradeInput.outerText));
-}
