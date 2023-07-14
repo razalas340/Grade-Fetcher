76d2877e52bf70ef5159fcd481db5a0f50f8bc4b
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-07-14 08:45:39 -0400

    debug

--- popup.js
+++ popup.js
@@ -8,29 +8,29 @@ function scrapGradesFromPage() {
   }
 
   // Send the data to SheetDB
-  sendDataToSheetDB(grades);
+  // sendDataToSheetDB(grades);
 
-  console.log(grades);
+  return grades;
 }
 
-function sendDataToSheetDB(data) {
-  const endpoint = "https://sheetdb.io/api/v1/p8ejp7owsd37x";
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  };
+// function sendDataToSheetDB(data) {
+//   const endpoint = "https://sheetdb.io/api/v1/p8ejp7owsd37x";
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify(data)
+//   };
 
-  fetch(endpoint, options)
-    .then(response => response.json())
-    .then(result => {
-      // Handle the response from SheetDB
-      console.log(result);
-    })
-    .catch(error => {
-      // Handle any errors
-      console.error(error);
-    });
-}
+//   fetch(endpoint, options)
+//     .then(response => response.json())
+//     .then(result => {
+//       // Handle the response from SheetDB
+//       console.log(result);
+//     })
+//     .catch(error => {
+//       // Handle any errors
+//       console.error(error);
+//     });
+// }
