9149ee88f9a4ded730d6c2bd809d339c7aefd608
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-07-14 09:21:27 -0400

    grabbing name

--- popup.js
+++ popup.js
@@ -18,12 +18,12 @@ scrapeGrades.addEventListener("click", async () => {
 
 function scrapEmailsFromPage() {
   let gradeInputs = document.querySelectorAll(".u7S8tc .ksaOtd");
-  let grades = [];
-  for (let i = 0; i < gradeInputs.length; i++) {
-    let grade = gradeInputs[i].innerHTML;
-    grades.push(grade);
-  }
-  return grades;
+  let studentName = document.querySelectorAll(".qp1Eye .B7SYid");
+
+  let name = studentName[0].innerHTML;
+  let score = gradeInputs[0].innerHTML;
+
+  return [name, score];
 }
 
 function sendDataToSheetDB(data) {
