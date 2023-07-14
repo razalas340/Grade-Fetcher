488b8e438e4734a9d1bab590c8cde8325b3ad6a6
razalas340 <135702134+razalas340@users.noreply.github.com>
2023-07-14 08:51:20 -0400

    new approach using documentation

--- popup.js
+++ popup.js
@@ -1,11 +1,45 @@
-function scrapGradesFromPage() {
+let container = document.getElementById("containerScore");
+let scrapeEmails = document.getElementById("scrapeEmails");
+
+scrapeEmails.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      func: scrapEmailsFromPage,
+    },
+    (injectionResults) => {
+      container.innerHTML = injectionResults[0].result;
+      sendDataToSheetDB(injectionResults[0].result);
+    }
+  );
+});
+
+function scrapEmailsFromPage() {
   let gradeInputs = document.querySelectorAll(".u7S8tc .ksaOtd");
-  let grades = [];
+  let score = gradeInputs[0].innerHTML;
+  return score;
+}
 
-  for (let i = 0; i < gradeInputs.length; i++) {
-    let grade = gradeInputs[i].innerHTML;
-    grades.push(grade);
-  }
-
-   return grades;
+function sendDataToSheetDB(data) {
+  console.log("data", data);
+  fetch("https://sheetdb.io/api/v1/p8ejp7owsd37x", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      data: [
+        {
+          score: data,
+          name: "Mark",
+          age: 18,
+        },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
